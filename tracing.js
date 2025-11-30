d4decd5997c3cf1ece29b2f32d80720692e095ee
Carmen Cruz Arana <carmenrubi.arana@gmail.com>
2025-11-30 18:28:02 -0500

    updated tracing.js to include otlp exporter

--- tracing.js
+++ tracing.js
@@ -2,31 +2,48 @@ const { resourceFromAttributes } = require('@opentelemetry/resources');
 const { ATTR_SERVICE_NAME } = require('@opentelemetry/semantic-conventions');
 const { NodeSDK } = require('@opentelemetry/sdk-node');
 const {
-getNodeAutoInstrumentations,
+  getNodeAutoInstrumentations,
 } = require('@opentelemetry/auto-instrumentations-node');
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
+
 const {
-ConsoleMetricExporter,
-PeriodicExportingMetricReader,
+  OTLPTraceExporter
+} = require('@opentelemetry/exporter-trace-otlp-http');
+
+const {
+  ConsoleMetricExporter,
+  PeriodicExportingMetricReader,
 } = require('@opentelemetry/sdk-metrics');
-//Instrumentations
+
 const { ExpressInstrumentation } =
-require("opentelemetry-instrumentation-express");
+  require("opentelemetry-instrumentation-express");
+
 const { MongoDBInstrumentation } =
-require("@opentelemetry/instrumentation-mongodb");
-const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
-const sdk = new NodeSDK({
-resource: resourceFromAttributes({
-[ATTR_SERVICE_NAME]: "todo-service"
-}),
-traceExporter: new ConsoleSpanExporter(),
-metricReader: new PeriodicExportingMetricReader({
-exporter: new ConsoleMetricExporter(),
-}),
-instrumentations: [
-getNodeAutoInstrumentations(),
-new ExpressInstrumentation(),
-new MongoDBInstrumentation(),
-new HttpInstrumentation()],
+  require("@opentelemetry/instrumentation-mongodb");
+
+const { HttpInstrumentation } =
+  require("@opentelemetry/instrumentation-http");
+
+// Use OTLP HTTP Exporter, pointed at Jaeger
+const traceExporter = new OTLPTraceExporter({
+  url: "http://localhost:4318/v1/traces"
 });
+
+const sdk = new NodeSDK({
+  resource: resourceFromAttributes({
+    [ATTR_SERVICE_NAME]: "todo-service"
+  }),
+  traceExporter: traceExporter,
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new ConsoleMetricExporter(),
+  }),
+  instrumentations: [
+    getNodeAutoInstrumentations(),
+    new ExpressInstrumentation(),
+    new MongoDBInstrumentation(),
+    new HttpInstrumentation()
+  ],
+});
+
 sdk.start();
+
+console.log("Tracing initialized using OTLP -> Jaeger");
